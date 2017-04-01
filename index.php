e93e8e635e125dcb3fb54701ecd72e40987ee666
sjazmin <sjazmin@users.noreply.github.com>
2017-04-01 11:16:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP Developer</title>
+    <link href="Assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="Assets/css/bootstrap-theme.min.css" rel="stylesheet" />
+    
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+            <h1>PHP Developer</h1>
+        </div>
+		<div class="row">
+            <div class="col-xs-12">
+        	
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Evaluación</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><a href="Parte1.php">Parte 1</a></td>
+					</tr>
+				</tbody>
+			</table>
+           
+            </div>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ parte1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP Developer</title>
+    <link href="Assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="Assets/css/bootstrap-theme.min.css" rel="stylesheet" />
+    
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron">
+            <h1>PHP Developer</h1>
+        </div>
+		<div class="row">
+            <div class="col-xs-12">
+        	<ol class="breadcrumb">
+			  <li><a href="index.php">Evaluación</a></li>
+			  <li class="active">Parte 1</li>
+			</ol>
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Parte 1</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><a href="Modulos/ejercicio1.php">Ejercicio 1</a></td>
+					</tr>
+					<tr>
+						<td><a href="Modulos/ejercicio2.php">Ejercicio 2</a></td>
+					</tr>
+					<tr>
+						<td><a href="Modulos/ejercicio3.php">Ejercicio 3</a></td>
+					</tr>
+					
+				</tbody>
+			</table>
+           
+            </div>
+		</div>
+	</div>
+
+</body>
+</html>
